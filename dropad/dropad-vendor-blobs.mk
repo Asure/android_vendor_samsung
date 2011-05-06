8ae61756c3f40857bcb2812b16c030066f018696
Alex Meijer <asure007@gmail.com>
2011-05-06 14:36:19 +0200

    Fixed internal audio

--- dropad/dropad-vendor-blobs.mk
+++ dropad/dropad-vendor-blobs.mk
@@ -22,7 +22,8 @@ PRODUCT_COPY_FILES := \
     vendor/samsung/dropad/proprietary/asound.conf:/system/etc/asound.conf \
     vendor/samsung/dropad/proprietary/asound0.conf:/system/etc/asound0.conf \
     vendor/samsung/dropad/proprietary/asound1.conf:/system/etc/asound1.conf \
-    vendor/samsung/dropad/proprietary/audio_sw.sh:/system/etc/audio_sw.sh
+    vendor/samsung/dropad/proprietary/audio_sw.sh:/system/etc/audio_sw.sh \
+    vendor/samsung/dropad/proprietary/libaudio.so:/system/lib/libaudio.so
 
 # Camera
 PRODUCT_COPY_FILES += \
